478f7ae41c7d5b9d3f50ef5db6ff236f5d10a5ec
roy <loitp@galaxyjoy.vn>
2024-11-06 14:11:12 +0700

    up

--- app/src/main/java/com/galaxyjoy/hexviewer/util/memory/MemoryListener.java
+++ app/src/main/java/com/galaxyjoy/hexviewer/util/memory/MemoryListener.java
@@ -1,17 +1,5 @@
 package com.galaxyjoy.hexviewer.util.memory;
 
-/**
- * ******************************************************************************
- * <p><b>Project HexViewer</b><br/>
- * Memory listener.
- * </p>
- *
- * @author Keidan
- * <p>
- * License: GPLv3
- * </p>
- * ******************************************************************************
- */
 public interface MemoryListener {
-  void onLowAppMemory(boolean disabled, MemoryInfo mi);
+    void onLowAppMemory(boolean disabled, MemoryInfo mi);
 }
