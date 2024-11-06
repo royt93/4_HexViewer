cdd0b06bf5a29665821754419ff910b325859c2b
roy <loitp@galaxyjoy.vn>
2024-11-06 15:53:10 +0700

    up

--- app/src/main/java/com/galaxyjoy/hexviewer/ui/payload/PayloadHexHelper.java
+++ app/src/main/java/com/galaxyjoy/hexviewer/ui/payload/PayloadHexHelper.java
@@ -7,153 +7,141 @@ import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import com.galaxyjoy.hexviewer.ui.adapters.HexTextArrayAdapter;
 import com.galaxyjoy.hexviewer.MyApplication;
 import com.galaxyjoy.hexviewer.R;
 import com.galaxyjoy.hexviewer.ui.activities.MainActivity;
+import com.galaxyjoy.hexviewer.ui.adapters.HexTextArrayAdapter;
 import com.galaxyjoy.hexviewer.ui.adapters.config.UserConfigLandscape;
 import com.galaxyjoy.hexviewer.ui.adapters.config.UserConfigPortrait;
 import com.galaxyjoy.hexviewer.ui.adapters.holders.LineNumbersTitle;
-import com.galaxyjoy.hexviewer.ui.multichoice.HexMultiChoiceCallback;
+import com.galaxyjoy.hexviewer.ui.multiChoice.HexMultiChoiceCallback;
 
-/**
- * ******************************************************************************
- * <p><b>Project HexViewer</b><br/>
- * Helper for the hex list view.
- * </p>
- *
- * @author Keidan
- * <p>
- * License: GPLv3
- * </p>
- * ******************************************************************************
- */
+import java.util.ArrayList;
+
 public class PayloadHexHelper {
-  private MainActivity mActivity;
-  private ListView mPayloadHex = null;
-  private HexTextArrayAdapter mAdapterHex = null;
-  private RelativeLayout mPayloadViewContainer = null;
-  private LinearLayout mTitle = null;
-  private TextView mTitleLineNumbers = null;
-  private TextView mTitleContent = null;
-  private MyApplication mApp = null;
+    private MainActivity mActivity;
+    private ListView mPayloadHex = null;
+    private HexTextArrayAdapter mAdapterHex = null;
+    private RelativeLayout mPayloadViewContainer = null;
+    private LinearLayout mTitle = null;
+    private TextView mTitleLineNumbers = null;
+    private TextView mTitleContent = null;
+    private MyApplication mApp = null;
 
-  /**
-   * Called when the activity is created.
-   *
-   * @param activity The owner activity
-   */
-  public void onCreate(final MainActivity activity) {
-    mActivity = activity;
-    mApp = (MyApplication) activity.getApplicationContext();
-    mPayloadViewContainer = activity.findViewById(R.id.payloadViewContainer);
-    mTitle = activity.findViewById(R.id.title);
-    mTitleLineNumbers = activity.findViewById(R.id.titleLineNumbers);
-    mTitleContent = activity.findViewById(R.id.titleContent);
-    mPayloadHex = activity.findViewById(R.id.payloadView);
+    /**
+     * Called when the activity is created.
+     *
+     * @param activity The owner activity
+     */
+    public void onCreate(final MainActivity activity) {
+        mActivity = activity;
+        mApp = (MyApplication) activity.getApplicationContext();
+        mPayloadViewContainer = activity.findViewById(R.id.payloadViewContainer);
+        mTitle = activity.findViewById(R.id.title);
+        mTitleLineNumbers = activity.findViewById(R.id.titleLineNumbers);
+        mTitleContent = activity.findViewById(R.id.titleContent);
+        mPayloadHex = activity.findViewById(R.id.payloadView);
 
-    mPayloadHex.setVisibility(View.GONE);
-    mPayloadViewContainer.setVisibility(View.GONE);
-    mTitleLineNumbers.setVisibility(View.GONE);
-    mTitleContent.setVisibility(View.GONE);
-    mTitle.setVisibility(View.GONE);
+        mPayloadHex.setVisibility(View.GONE);
+        mPayloadViewContainer.setVisibility(View.GONE);
+        mTitleLineNumbers.setVisibility(View.GONE);
+        mTitleContent.setVisibility(View.GONE);
+        mTitle.setVisibility(View.GONE);
 
-    LineNumbersTitle title = new LineNumbersTitle();
-    title.setTitleContent(mTitleContent);
-    title.setTitleLineNumbers(mTitleLineNumbers);
+        LineNumbersTitle title = new LineNumbersTitle();
+        title.setTitleContent(mTitleContent);
+        title.setTitleLineNumbers(mTitleLineNumbers);
 
-    mAdapterHex = new HexTextArrayAdapter(activity,
-      new ArrayList<>(),
-      title,
-      new UserConfigPortrait(activity, true),
-      new UserConfigLandscape(activity, true));
-    mPayloadHex.setAdapter(mAdapterHex);
-    mPayloadHex.setOnItemClickListener(activity);
-    mPayloadHex.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
-    HexMultiChoiceCallback hexMultiChoiceCallback = new HexMultiChoiceCallback(activity, mPayloadHex, mAdapterHex);
-    mPayloadHex.setMultiChoiceModeListener(hexMultiChoiceCallback);
-  }
+        mAdapterHex = new HexTextArrayAdapter(activity,
+                new ArrayList<>(),
+                title,
+                new UserConfigPortrait(activity, true),
+                new UserConfigLandscape(activity, true));
+        mPayloadHex.setAdapter(mAdapterHex);
+        mPayloadHex.setOnItemClickListener(activity);
+        mPayloadHex.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        HexMultiChoiceCallback hexMultiChoiceCallback = new HexMultiChoiceCallback(activity, mPayloadHex, mAdapterHex);
+        mPayloadHex.setMultiChoiceModeListener(hexMultiChoiceCallback);
+    }
 
-  /**
-   * Resets the update status.
-   */
-  public void resetUpdateStatus() {
-    String query = mActivity.getSearchQuery();
-    if (!query.isEmpty())
-      mAdapterHex.manualFilterUpdate(""); /* reset filter */
-    mAdapterHex.getEntries().clearFilteredUpdated();
-    if (!query.isEmpty())
-      mAdapterHex.manualFilterUpdate(query);
-    mAdapterHex.notifyDataSetChanged();
-  }
+    /**
+     * Resets the update status.
+     */
+    public void resetUpdateStatus() {
+        String query = mActivity.getSearchQuery();
+        if (!query.isEmpty())
+            mAdapterHex.manualFilterUpdate(""); /* reset filter */
+        mAdapterHex.getEntries().clearFilteredUpdated();
+        if (!query.isEmpty())
+            mAdapterHex.manualFilterUpdate(query);
+        mAdapterHex.notifyDataSetChanged();
+    }
 
-  /**
-   * Called to refresh the adapter.
-   */
-  public void refreshAdapter() {
-    mAdapterHex.refresh();
-  }
+    /**
+     * Called to refresh the adapter.
+     */
+    public void refreshAdapter() {
+        mAdapterHex.refresh();
+    }
 
-  /**
-   * Called to refresh the line numbers.
-   */
-  public void refreshLineNumbers() {
-    refreshLineNumbersVisibility();
-    mAdapterHex.notifyDataSetChanged();
-  }
+    /**
+     * Called to refresh the line numbers.
+     */
+    public void refreshLineNumbers() {
+        refreshLineNumbersVisibility();
+        mAdapterHex.notifyDataSetChanged();
+    }
 
-  /**
-   * Returns the hex adapter.
-   *
-   * @return HexTextArrayAdapter
-   */
-  public HexTextArrayAdapter getAdapter() {
-    return mAdapterHex;
-  }
+    /**
+     * Returns the hex adapter.
+     *
+     * @return HexTextArrayAdapter
+     */
+    public HexTextArrayAdapter getAdapter() {
+        return mAdapterHex;
+    }
 
-  /**
-   * Tests if the list view is visible.
-   *
-   * @return boolean
-   */
-  public boolean isVisible() {
-    return mPayloadHex.getVisibility() == View.VISIBLE;
-  }
+    /**
+     * Tests if the list view is visible.
+     *
+     * @return boolean
+     */
+    public boolean isVisible() {
+        return mPayloadHex.getVisibility() == View.VISIBLE;
+    }
 
-  /**
-   * Changes the list view visibility.
-   *
-   * @param b The new value
-   */
-  public void setVisible(boolean b) {
-    mPayloadHex.setVisibility(b ? View.VISIBLE : View.GONE);
-    mPayloadViewContainer.setVisibility(b ? View.VISIBLE : View.GONE);
-    if (!b) {
-      mTitleLineNumbers.setVisibility(View.GONE);
-      mTitleContent.setVisibility(View.GONE);
-      mTitle.setVisibility(View.GONE);
-    } else
-      refreshLineNumbersVisibility();
-  }
+    /**
+     * Changes the list view visibility.
+     *
+     * @param b The new value
+     */
+    public void setVisible(boolean b) {
+        mPayloadHex.setVisibility(b ? View.VISIBLE : View.GONE);
+        mPayloadViewContainer.setVisibility(b ? View.VISIBLE : View.GONE);
+        if (!b) {
+            mTitleLineNumbers.setVisibility(View.GONE);
+            mTitleContent.setVisibility(View.GONE);
+            mTitle.setVisibility(View.GONE);
+        } else
+            refreshLineNumbersVisibility();
+    }
 
-  /**
-   * Refreshes line numbers visibility.
-   */
-  private void refreshLineNumbersVisibility() {
-    final boolean checked = mApp.isLineNumber();
-    mTitleLineNumbers.setVisibility(checked ? View.VISIBLE : View.GONE);
-    mTitleContent.setVisibility(checked ? View.VISIBLE : View.GONE);
-    mTitle.setVisibility(checked ? View.VISIBLE : View.GONE);
-  }
+    /**
+     * Refreshes line numbers visibility.
+     */
+    private void refreshLineNumbersVisibility() {
+        final boolean checked = mApp.isLineNumber();
+        mTitleLineNumbers.setVisibility(checked ? View.VISIBLE : View.GONE);
+        mTitleContent.setVisibility(checked ? View.VISIBLE : View.GONE);
+        mTitle.setVisibility(checked ? View.VISIBLE : View.GONE);
+    }
 
-  /**
-   * Returns the ListView
-   *
-   * @return ListView
-   */
-  public ListView getListView() {
-    return mPayloadHex;
-  }
+    /**
+     * Returns the ListView
+     *
+     * @return ListView
+     */
+    public ListView getListView() {
+        return mPayloadHex;
+    }
 }
--- app/src/main/java/com/galaxyjoy/hexviewer/ui/popup/MainPopupWindow.java
+++ app/src/main/java/com/galaxyjoy/hexviewer/ui/popup/MainPopupWindow.java
@@ -16,183 +16,174 @@ import com.galaxyjoy.hexviewer.R;
 import com.galaxyjoy.hexviewer.ui.undoredo.UnDoRedo;
 import com.galaxyjoy.hexviewer.util.SysHelper;
 
-/**
- * ******************************************************************************
- * <p><b>Project HexViewer</b><br/>
- * PopupWindow used with MainActivity.
- * </p>
- *
- * @author Keidan
- * <p>
- * License: GPLv3
- * </p>
- * ******************************************************************************
- */
 public class MainPopupWindow {
-  private final PopupWindow mPopup;
-  private final TextView mGoTo;
-  private final TextView mSaveMenu;
-  private final TextView mSaveAsMenu;
-  private final TextView mCloseMenu;
-  private final TextView mRecentlyOpen;
-  private final PopupCheckboxHelper mPlainText;
-  private final PopupCheckboxHelper mLineNumbers;
-  private final MyApplication mApp;
+    private final PopupWindow mPopup;
+    private final TextView mGoTo;
+    private final TextView mSaveMenu;
+    private final TextView mSaveAsMenu;
+    private final TextView mCloseMenu;
+    private final TextView mRecentlyOpen;
+    private final PopupCheckboxHelper mPlainText;
+    private final PopupCheckboxHelper mLineNumbers;
+    private final MyApplication mApp;
 
-  public interface ClickListener {
-    void onClick(int id);
-  }
-
-  public MainPopupWindow(final Context ctx, UnDoRedo undoRedo, ClickListener clickListener) {
-    mApp = (MyApplication) ctx.getApplicationContext();
-
-    LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    @SuppressLint("InflateParams") View popupView = inflater.inflate(R.layout.main_popup, null);
-    popupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-    int width = popupView.getMeasuredWidth();
-
-    mPopup = new PopupWindow(popupView,
-      width + 150,
-      ViewGroup.LayoutParams.WRAP_CONTENT, true);
-
-    mPopup.setElevation(5.0f);
-    mPopup.setOutsideTouchable(true);
-
-    mPlainText = new PopupCheckboxHelper(popupView,
-      R.id.action_plain_text_container,
-      R.id.action_plain_text_tv,
-      R.id.action_plain_text_cb);
-
-    mLineNumbers = new PopupCheckboxHelper(popupView,
-      R.id.action_line_numbers_container,
-      R.id.action_line_numbers_tv,
-      R.id.action_line_numbers_cb);
-    mSaveAsMenu = popupView.findViewById(R.id.action_save_as);
-    mSaveMenu = popupView.findViewById(R.id.action_save);
-    mCloseMenu = popupView.findViewById(R.id.action_close);
-    mRecentlyOpen = popupView.findViewById(R.id.action_recently_open);
-    ImageView actionRedo = popupView.findViewById(R.id.action_redo);
-    ImageView actionUndo = popupView.findViewById(R.id.action_undo);
-    FrameLayout containerRedo = popupView.findViewById(R.id.containerRedo);
-    FrameLayout containerUndo = popupView.findViewById(R.id.containerUndo);
-    mGoTo = popupView.findViewById(R.id.action_go_to);
-
-    View.OnClickListener click = v -> {
-      mPopup.dismiss();
-      if (clickListener != null)
-        clickListener.onClick(v.getId());
-    };
-    popupView.findViewById(R.id.action_open).setOnClickListener(click);
-    popupView.findViewById(R.id.action_open_sequential).setOnClickListener(click);
-    popupView.findViewById(R.id.action_settings).setOnClickListener(click);
-    mPlainText.setOnClickListener(click);
-    mLineNumbers.setOnClickListener(click);
-    mSaveAsMenu.setOnClickListener(click);
-    mSaveMenu.setOnClickListener(click);
-    mCloseMenu.setOnClickListener(click);
-    mRecentlyOpen.setOnClickListener(click);
-    mGoTo.setOnClickListener(click);
-    actionRedo.setOnClickListener(click);
-    actionUndo.setOnClickListener(click);
-    undoRedo.setControls(containerUndo, actionUndo, containerRedo, actionRedo);
-    mLineNumbers.setChecked(mApp.isLineNumber());
-
-    refreshGoToName();
-  }
-
-  /**
-   * Shows the popup
-   *
-   * @param more Button "more"
-   */
-  public void show(View more) {
-    if (mPopup != null) {
-      mPopup.showAtLocation(more, Gravity.TOP | (SysHelper.isRTL(more) ? Gravity.START : Gravity.END), 12, 120);
+    public interface ClickListener {
+        void onClick(int id);
     }
-  }
 
-  /**
-   * Dismiss the popup.
-   */
-  public void dismiss() {
-    if (mPopup != null && mPopup.isShowing())
-      mPopup.dismiss();
-  }
+    public MainPopupWindow(final Context ctx,
+                           UnDoRedo undoRedo,
+                           ClickListener clickListener) {
+        mApp = (MyApplication) ctx.getApplicationContext();
 
-  /**
-   * Returns the menu RecentlyOpen
-   *
-   * @return MenuItem
-   */
-  public TextView getMenuRecentlyOpen() {
-    return mRecentlyOpen;
-  }
+        LayoutInflater inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        @SuppressLint("InflateParams") View popupView = inflater.inflate(R.layout.main_popup, null);
+        popupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        int width = popupView.getMeasuredWidth();
 
-  /**
-   * Enables/Disables the save as menu.
-   *
-   * @param en boolean
-   */
-  public void setMenusEnable(boolean en) {
-    setMenuEnabled(mGoTo, en);
-    setMenuEnabled(mSaveAsMenu, en);
-    setMenuEnabled(mCloseMenu, en);
-    setMenuEnabled(mRecentlyOpen, !mApp.getRecentlyOpened().list().isEmpty());
-    if (mLineNumbers != null)
-      mLineNumbers.setEnable(en);
-    if (!en && mPlainText != null) {
-      mPlainText.setChecked(false);
-      mPlainText.setEnable(false);
+        mPopup = new PopupWindow(popupView,
+                width + 150,
+                ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+        mPopup.setElevation(5.0f);
+        mPopup.setOutsideTouchable(true);
+
+        mPlainText = new PopupCheckboxHelper(popupView,
+                R.id.action_plain_text_container,
+                R.id.action_plain_text_tv,
+                R.id.action_plain_text_cb);
+
+        mLineNumbers = new PopupCheckboxHelper(popupView,
+                R.id.action_line_numbers_container,
+                R.id.action_line_numbers_tv,
+                R.id.action_line_numbers_cb);
+        mSaveAsMenu = popupView.findViewById(R.id.action_save_as);
+        mSaveMenu = popupView.findViewById(R.id.action_save);
+        mCloseMenu = popupView.findViewById(R.id.action_close);
+        mRecentlyOpen = popupView.findViewById(R.id.action_recently_open);
+        ImageView actionRedo = popupView.findViewById(R.id.action_redo);
+        ImageView actionUndo = popupView.findViewById(R.id.action_undo);
+        FrameLayout containerRedo = popupView.findViewById(R.id.containerRedo);
+        FrameLayout containerUndo = popupView.findViewById(R.id.containerUndo);
+        mGoTo = popupView.findViewById(R.id.action_go_to);
+
+        View.OnClickListener click = v -> {
+            mPopup.dismiss();
+            if (clickListener != null)
+                clickListener.onClick(v.getId());
+        };
+        popupView.findViewById(R.id.action_open).setOnClickListener(click);
+        popupView.findViewById(R.id.action_open_sequential).setOnClickListener(click);
+        popupView.findViewById(R.id.action_settings).setOnClickListener(click);
+        mPlainText.setOnClickListener(click);
+        mLineNumbers.setOnClickListener(click);
+        mSaveAsMenu.setOnClickListener(click);
+        mSaveMenu.setOnClickListener(click);
+        mCloseMenu.setOnClickListener(click);
+        mRecentlyOpen.setOnClickListener(click);
+        mGoTo.setOnClickListener(click);
+        actionRedo.setOnClickListener(click);
+        actionUndo.setOnClickListener(click);
+        undoRedo.setControls(containerUndo, actionUndo, containerRedo, actionRedo);
+        mLineNumbers.setChecked(mApp.isLineNumber());
+
+        refreshGoToName();
     }
-  }
 
-  /**
-   * Refreshes go to menu.
-   */
-  public void refreshGoToName() {
-    if (mGoTo != null) {
-      if (mLineNumbers != null && mLineNumbers.isChecked())
-        mGoTo.setText(R.string.action_go_to_address);
-      else
-        mGoTo.setText(R.string.action_go_to_line);
+    /**
+     * Shows the popup
+     *
+     * @param more Button "more"
+     */
+    public void show(View more) {
+        if (mPopup != null) {
+            mPopup.showAtLocation(more, Gravity.TOP | (SysHelper.isRTL(more) ? Gravity.START : Gravity.END), 12, 120);
+        }
     }
-  }
 
-  /**
-   * Enables/Disables the save menu.
-   *
-   * @param en boolean
-   */
-  public void setSaveMenuEnable(boolean en) {
-    setMenuEnabled(mSaveMenu, en);
-  }
+    /**
+     * Dismiss the popup.
+     */
+    public void dismiss() {
+        if (mPopup != null && mPopup.isShowing())
+            mPopup.dismiss();
+    }
 
-  /**
-   * Sets whether the menu item is enabled.
-   *
-   * @param menu    MenuItem
-   * @param enabled If true then the item will be invokable; if false it is won't be invokable.
-   */
-  private void setMenuEnabled(final TextView menu, final boolean enabled) {
-    if (menu != null)
-      menu.setEnabled(enabled);
-  }
+    /**
+     * Returns the menu RecentlyOpen
+     *
+     * @return MenuItem
+     */
+    public TextView getMenuRecentlyOpen() {
+        return mRecentlyOpen;
+    }
 
-  /**
-   * Returns PopupCheckboxHelper for plain text.
-   *
-   * @return PopupCheckboxHelper
-   */
-  public PopupCheckboxHelper getPlainText() {
-    return mPlainText;
-  }
+    /**
+     * Enables/Disables the save as menu.
+     *
+     * @param en boolean
+     */
+    public void setMenusEnable(boolean en) {
+        setMenuEnabled(mGoTo, en);
+        setMenuEnabled(mSaveAsMenu, en);
+        setMenuEnabled(mCloseMenu, en);
+        setMenuEnabled(mRecentlyOpen, !mApp.getRecentlyOpened().list().isEmpty());
+        if (mLineNumbers != null)
+            mLineNumbers.setEnable(en);
+        if (!en && mPlainText != null) {
+            mPlainText.setChecked(false);
+            mPlainText.setEnable(false);
+        }
+    }
 
-  /**
-   * Returns PopupCheckboxHelper for line numbers.
-   *
-   * @return PopupCheckboxHelper
-   */
-  public PopupCheckboxHelper getLineNumbers() {
-    return mLineNumbers;
-  }
+    /**
+     * Refreshes go to menu.
+     */
+    public void refreshGoToName() {
+        if (mGoTo != null) {
+            if (mLineNumbers != null && mLineNumbers.isChecked())
+                mGoTo.setText(R.string.action_go_to_address);
+            else
+                mGoTo.setText(R.string.action_go_to_line);
+        }
+    }
+
+    /**
+     * Enables/Disables the save menu.
+     *
+     * @param en boolean
+     */
+    public void setSaveMenuEnable(boolean en) {
+        setMenuEnabled(mSaveMenu, en);
+    }
+
+    /**
+     * Sets whether the menu item is enabled.
+     *
+     * @param menu    MenuItem
+     * @param enabled If true then the item will be invokable; if false it is won't be invokable.
+     */
+    private void setMenuEnabled(final TextView menu,
+                                final boolean enabled) {
+        if (menu != null)
+            menu.setEnabled(enabled);
+    }
+
+    /**
+     * Returns PopupCheckboxHelper for plain text.
+     *
+     * @return PopupCheckboxHelper
+     */
+    public PopupCheckboxHelper getPlainText() {
+        return mPlainText;
+    }
+
+    /**
+     * Returns PopupCheckboxHelper for line numbers.
+     *
+     * @return PopupCheckboxHelper
+     */
+    public PopupCheckboxHelper getLineNumbers() {
+        return mLineNumbers;
+    }
 }
