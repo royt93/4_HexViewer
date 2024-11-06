5e5b70d758454d20eb9d81c9d7d5ecf578a39d17
roy <loitp@galaxyjoy.vn>
2024-11-06 16:20:35 +0700

    up

--- app/src/main/java/com/galaxyjoy/hexviewer/ui/launcher/LauncherOpen.java
+++ app/src/main/java/com/galaxyjoy/hexviewer/ui/launcher/LauncherOpen.java
@@ -18,90 +18,88 @@ import com.galaxyjoy.hexviewer.util.io.FileHelper;
 import com.galaxyjoy.hexviewer.MyApplication;
 import com.galaxyjoy.hexviewer.R;
 
-/**
- * ******************************************************************************
- * <p><b>Project HexViewer</b><br/>
- * Launcher used with the open part.
- * </p>
- *
- * @author Keidan
- * <p>
- * License: GPLv3
- * </p>
- * ******************************************************************************
- */
 public class LauncherOpen {
-  private final MainActivity mActivity;
-  private final LinearLayout mMainLayout;
-  private final MyApplication mApp;
-  private ActivityResultLauncher<Intent> activityResultLauncherOpen;
+    private final MainActivity mActivity;
+    private final LinearLayout mMainLayout;
+    private final MyApplication mApp;
+    private ActivityResultLauncher<Intent> activityResultLauncherOpen;
 
-  public LauncherOpen(MainActivity activity, LinearLayout mainLayout) {
-    mActivity = activity;
-    mApp = (MyApplication) activity.getApplicationContext();
-    mMainLayout = mainLayout;
-    register();
-  }
-
-  /**
-   * Starts the activity.
-   */
-  public void startActivity() {
-    UIHelper.openFilePickerInFileSelectionMode(mActivity, activityResultLauncherOpen, mMainLayout);
-  }
-
-  /**
-   * Registers result launcher for the activity for opening a file.
-   */
-  private void register() {
-    activityResultLauncherOpen = mActivity.registerForActivityResult(
-      new ActivityResultContracts.StartActivityForResult(),
-      result -> {
-        if (result.getResultCode() == Activity.RESULT_OK) {
-          Intent data = result.getData();
-          if (data != null) {
-            if (FileHelper.takeUriPermissions(mActivity, data.getData(), false)) {
-              processFileOpen(new FileData(mActivity, data.getData(), false, 0L, 0L));
-            } else
-              UIHelper.showErrorDialog(mActivity, R.string.error_title, String.format(mActivity.getString(R.string.error_file_permission), FileHelper.getFileName(mApp, data.getData())));
-          } else {
-            Log.e(getClass().getSimpleName(), "Null data!!!");
-            mApp.setSequential(false);
-          }
-        } else
-          mApp.setSequential(false);
-      });
-  }
-
-  /**
-   * Process the opening of the file
-   */
-  private void processFileOpen(final FileData fd) {
-    processFileOpen(fd, null, true);
-  }
-
-
-  /**
-   * Process the opening of the file
-   *
-   * @param fd FileData.
-   */
-  public void processFileOpen(final FileData fd, final String oldToString, final boolean addRecent) {
-    if (fd != null && fd.getUri() != null && fd.getUri().getPath() != null) {
-      final FileData previous = mActivity.getFileData();
-      mActivity.setFileData(fd);
-      Runnable r = () -> {
-        mActivity.getUnDoRedo().clear();
-        MyApplication.addLog(mActivity, "Open",
-          String.format(Locale.US, "Open file: '%s'", mActivity.getFileData()));
-        new TaskOpen(mActivity, mActivity.getPayloadHex().getAdapter(), mActivity, oldToString, addRecent).execute(mActivity.getFileData());
-      };
-      if (mApp.isSequential())
-        mActivity.getLauncherPartialOpen().startActivity(previous, oldToString, addRecent);
-      else
-        r.run();
-    } else {
-      UIHelper.showErrorDialog(mActivity, R.string.error_title, mActivity.getString(R.string.error_filename));
+    public LauncherOpen(MainActivity activity, LinearLayout mainLayout) {
+        mActivity = activity;
+        mApp = (MyApplication) activity.getApplicationContext();
+        mMainLayout = mainLayout;
+        register();
     }
-  }
+
+    /**
+     * Starts the activity.
+     */
+    public void startActivity() {
+        UIHelper.openFilePickerInFileSelectionMode(mActivity,
+                activityResultLauncherOpen,
+                mMainLayout);
+    }
+
+    /**
+     * Registers result launcher for the activity for opening a file.
+     */
+    private void register() {
+        activityResultLauncherOpen = mActivity.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            if (FileHelper.takeUriPermissions(mActivity,
+                                    data.getData(),
+                                    false)) {
+                                processFileOpen(new FileData(mActivity,
+                                        data.getData(),
+                                        false,
+                                        0L,
+                                        0L));
+                            } else
+                                UIHelper.showErrorDialog(mActivity,
+                                        R.string.error_title,
+                                        String.format(mActivity.getString(R.string.error_file_permission), FileHelper.getFileName(mApp, data.getData())));
+                        } else {
+                            Log.e(getClass().getSimpleName(), "Null data!!!");
+                            mApp.setSequential(false);
+                        }
+                    } else
+                        mApp.setSequential(false);
+                });
+    }
+
+    /**
+     * Process the opening of the file
+     */
+    private void processFileOpen(final FileData fd) {
+        processFileOpen(fd, null, true);
+    }
+
+
+    /**
+     * Process the opening of the file
+     *
+     * @param fd FileData.
+     */
+    public void processFileOpen(final FileData fd, final String oldToString, final boolean addRecent) {
+        if (fd != null && fd.getUri() != null && fd.getUri().getPath() != null) {
+            final FileData previous = mActivity.getFileData();
+            mActivity.setFileData(fd);
+            Runnable r = () -> {
+                mActivity.getUnDoRedo().clear();
+                MyApplication.addLog(mActivity, "Open",
+                        String.format(Locale.US, "Open file: '%s'", mActivity.getFileData()));
+                new TaskOpen(mActivity, mActivity.getPayloadHex().getAdapter(), mActivity, oldToString, addRecent).execute(mActivity.getFileData());
+            };
+            if (mApp.isSequential())
+                mActivity.getLauncherPartialOpen().startActivity(previous, oldToString, addRecent);
+            else
+                r.run();
+        } else {
+            UIHelper.showErrorDialog(mActivity, R.string.error_title, mActivity.getString(R.string.error_filename));
+        }
+    }
 }
--- app/src/main/java/com/galaxyjoy/hexviewer/ui/launcher/LauncherPartialOpen.java
+++ app/src/main/java/com/galaxyjoy/hexviewer/ui/launcher/LauncherPartialOpen.java
@@ -8,82 +8,76 @@ import android.util.Log;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 
+import com.galaxyjoy.hexviewer.MyApplication;
 import com.galaxyjoy.hexviewer.models.FileData;
 import com.galaxyjoy.hexviewer.ui.activities.MainActivity;
 import com.galaxyjoy.hexviewer.ui.activities.PartialOpenActivity;
 import com.galaxyjoy.hexviewer.ui.task.TaskOpen;
-import com.galaxyjoy.hexviewer.MyApplication;
 
-/**
- * ******************************************************************************
- * <p><b>Project HexViewer</b><br/>
- * Launcher used with the partial open part.
- * </p>
- *
- * @author Keidan
- * <p>
- * License: GPLv3
- * </p>
- * ******************************************************************************
- */
 public class LauncherPartialOpen {
-  private final MainActivity mActivity;
-  private FileData mPrevious;
-  private boolean mAddRecent;
-  private String mOldToString;
-  private ActivityResultLauncher<Intent> activityResultLauncherOpen;
-  private final MyApplication mApp;
+    private final MainActivity mActivity;
+    private FileData mPrevious;
+    private boolean mAddRecent;
+    private String mOldToString;
+    private ActivityResultLauncher<Intent> activityResultLauncherOpen;
+    private final MyApplication mApp;
 
-  public LauncherPartialOpen(MainActivity activity) {
-    mActivity = activity;
-    mApp = (MyApplication) activity.getApplicationContext();
-    register();
-  }
+    public LauncherPartialOpen(MainActivity activity) {
+        mActivity = activity;
+        mApp = (MyApplication) activity.getApplicationContext();
+        register();
+    }
 
-  /**
-   * Starts the activity.
-   */
-  public void startActivity(final FileData previous, final String oldToString, final boolean addRecent) {
-    mPrevious = previous;
-    mAddRecent = addRecent;
-    mOldToString = oldToString;
-    PartialOpenActivity.startActivity(mActivity, activityResultLauncherOpen, mActivity.getFileData());
-  }
+    /**
+     * Starts the activity.
+     */
+    public void startActivity(final FileData previous,
+                              final String oldToString,
+                              final boolean addRecent) {
+        mPrevious = previous;
+        mAddRecent = addRecent;
+        mOldToString = oldToString;
+        PartialOpenActivity.startActivity(mActivity,
+                activityResultLauncherOpen,
+                mActivity.getFileData());
+    }
 
-  /**
-   * Registers result launcher for the activity for opening a file.
-   */
-  private void register() {
-    Runnable cancel = () -> {
-      mApp.setSequential(false);
-      if (mPrevious == null) {
-        mActivity.onOpenResult(false, false);
-      } else {
-        mActivity.setFileData(mPrevious);
-        mActivity.refreshTitle();
-      }
-    };
-    activityResultLauncherOpen = mActivity.registerForActivityResult(
-      new ActivityResultContracts.StartActivityForResult(),
-      result -> {
-        mApp.setSequential(false);
-        if (result.getResultCode() == Activity.RESULT_OK) {
-          Intent data = result.getData();
-          if (data != null) {
-            Bundle bundle = data.getExtras();
-            final long startOffset = bundle.getLong(PartialOpenActivity.RESULT_START_OFFSET);
-            final long endOffset = bundle.getLong(PartialOpenActivity.RESULT_END_OFFSET);
-            mActivity.getFileData().setOffsets(startOffset, endOffset, endOffset != 0L);
-            mActivity.getUnDoRedo().clear();
-            new TaskOpen(mActivity, mActivity.getPayloadHex().getAdapter(), mActivity, mOldToString, mAddRecent).execute(mActivity.getFileData());
-          } else {
-            Log.e(getClass().getSimpleName(), "LauncherPartialOpen -> Invalid data object!!!");
-            MyApplication.addLog(mActivity, "PartialOpen", "Null intent data!");
-            cancel.run();
-          }
-        } else {
-          cancel.run();
-        }
-      });
-  }
+    /**
+     * Registers result launcher for the activity for opening a file.
+     */
+    private void register() {
+        Runnable cancel = () -> {
+            mApp.setSequential(false);
+            if (mPrevious == null) {
+                mActivity.onOpenResult(false, false);
+            } else {
+                mActivity.setFileData(mPrevious);
+                mActivity.refreshTitle();
+            }
+        };
+        activityResultLauncherOpen = mActivity.registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    mApp.setSequential(false);
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            Bundle bundle = data.getExtras();
+                            final long startOffset = bundle.getLong(PartialOpenActivity.RESULT_START_OFFSET);
+                            final long endOffset = bundle.getLong(PartialOpenActivity.RESULT_END_OFFSET);
+                            mActivity.getFileData().setOffsets(startOffset, endOffset, endOffset != 0L);
+                            mActivity.getUnDoRedo().clear();
+                            new TaskOpen(mActivity, mActivity.getPayloadHex().getAdapter(), mActivity, mOldToString, mAddRecent).execute(mActivity.getFileData());
+                        } else {
+                            Log.e(getClass().getSimpleName(), "LauncherPartialOpen -> Invalid data object!!!");
+                            MyApplication.addLog(mActivity,
+                                    "PartialOpen",
+                                    "Null intent data!");
+                            cancel.run();
+                        }
+                    } else {
+                        cancel.run();
+                    }
+                });
+    }
 }
