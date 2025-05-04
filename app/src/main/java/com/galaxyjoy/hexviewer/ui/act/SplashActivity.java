2db4f743990b4e21c7747ac528e529aae2df1f28
roy <loitp@galaxyjoy.vn>
2025-05-04 21:20:59 +0700

    up

--- app/src/main/java/com/galaxyjoy/hexviewer/ui/act/SplashActivity.java
+++ app/src/main/java/com/galaxyjoy/hexviewer/ui/act/SplashActivity.java
@@ -17,6 +17,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function0;
+import kotlin.jvm.functions.Function1;
 
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
@@ -29,42 +30,56 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void checkShowAd() {
-        final Handler handler = new Handler(Looper.getMainLooper());
-        final AtomicBoolean hasCalledGoToMain = new AtomicBoolean(false);
-
-        new Thread(() -> {
-            final Runnable delayedRunnable = () -> {
-                if (hasCalledGoToMain.compareAndSet(false, true)) {
-                    Log.d("roy93~", "goToMain #1");
-                    runOnUiThread(this::goToMain);
-                }
-            };
-
-            handler.postDelayed(delayedRunnable, 3000);
-
-            // Load quảng cáo
-            runOnUiThread(() -> AdMobManager.INSTANCE.loadAppOpenAd(
-                    SplashActivity.this,
-                    BuildConfig.ADMOB_APP_OPEN_ID,
-                    new Function0<Unit>() {
-                        @Override
-                        public Unit invoke() {
-                            if (hasCalledGoToMain.compareAndSet(false, true)) {
-                                handler.removeCallbacks(delayedRunnable);
-                                Log.d("roy93~", "goToMain #2");
-                                runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        goToMain();
-                                        AdMobManager.INSTANCE.showAppOpenAd(SplashActivity.this);
-                                    }
-                                });
-                            }
-                            return null;
-                        }
+        AdMobManager.INSTANCE.loadAppOpenAd(
+                SplashActivity.this,
+                BuildConfig.ADMOB_APP_OPEN_ID,
+                new Function1<Boolean, Unit>() {
+                    @Override
+                    public Unit invoke(Boolean aBoolean) {
+                        Log.d("roy93~", "loadAppOpenAd aBoolean " + aBoolean);
+                        goToMain();
+                        AdMobManager.INSTANCE.showAppOpenAd(SplashActivity.this);
+                        return null;
                     }
-            ));
-        }).start();
+                }
+        );
+
+//        final Handler handler = new Handler(Looper.getMainLooper());
+//        final AtomicBoolean hasCalledGoToMain = new AtomicBoolean(false);
+//
+//        new Thread(() -> {
+//            final Runnable delayedRunnable = () -> {
+//                if (hasCalledGoToMain.compareAndSet(false, true)) {
+//                    Log.d("roy93~", "goToMain #1");
+//                    runOnUiThread(this::goToMain);
+//                }
+//            };
+//
+//            handler.postDelayed(delayedRunnable, 3000);
+//
+//            // Load quảng cáo
+//            runOnUiThread(() -> AdMobManager.INSTANCE.loadAppOpenAd(
+//                    SplashActivity.this,
+//                    BuildConfig.ADMOB_APP_OPEN_ID,
+//                    new Function0<Unit>() {
+//                        @Override
+//                        public Unit invoke() {
+//                            if (hasCalledGoToMain.compareAndSet(false, true)) {
+//                                handler.removeCallbacks(delayedRunnable);
+//                                Log.d("roy93~", "goToMain #2");
+//                                runOnUiThread(new Runnable() {
+//                                    @Override
+//                                    public void run() {
+//                                        goToMain();
+//                                        AdMobManager.INSTANCE.showAppOpenAd(SplashActivity.this);
+//                                    }
+//                                });
+//                            }
+//                            return null;
+//                        }
+//                    }
+//            ));
+//        }).start();
     }
 
     private void goToMain() {
